6b685829957e9a9bb06b9571a38f82ddefed2f78
lucia <69209369+Lls28es@users.noreply.github.com>
2021-08-04 18:47:58 -0300

    index a truncate

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ truncate: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`API listening at ${PORT}`); // eslint-disable-line no-console
   });
